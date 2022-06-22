8a058aac7f2133a40192670d527585d8646daf2e
Adithya Shenoy <adithyashenoy6@gmail.com>
2022-06-22 12:05:51 +0530

    Entitlement script addded

---
+++ Entitlement/dml_script_22-6-2022-0-5-50.sql
@@ -0,0 +1,5 @@
+delete from WPP_ENTL_PRMSN_OVRD where ENTL_NM='CareMgmt-ClinDxHealthCond' and guid in (12345);
+
+INSERT INTO WPP_ENTL_PRMSN_OVRD (WPP_ENTL_PRMSN_OVRD_IK,RGN,GUID,ENTL_PRMSN,ENTL_NM,ENTL_SRC_ID,PILOT_APP_NAME,CRTE_DT_TM,MOD_DT_TM)VALUES (null,'Southern California (SCA)','12345','DENY','CareMgmt-ClinDxHealthCond','KP_EBIZ','CareMgmt-ClinDxHealthCond',SYSTIMESTAMP,SYSTIMESTAMP);
+
+commit;
