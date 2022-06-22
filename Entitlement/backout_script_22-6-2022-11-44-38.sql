002ccdceee7e86b651b5d59ac910bf9f45fa79e0
Adithya Shenoy <adithyashenoy6@gmail.com>
2022-06-22 11:44:40 +0530

    Entitlement script addded

---
+++ Entitlement/backout_script_22-6-2022-11-44-38.sql
@@ -0,0 +1,3 @@
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='CareMgmt-ClinDxHealthCond') and RGN_ID in ('SCA');
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='CareMgmt-CondMgmt-Immunizations') and RGN_ID in ('MID');
+commit;
