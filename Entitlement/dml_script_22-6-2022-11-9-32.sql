2fd1d247a64a79c7e64bd9b86898cd9a1af50b9d
Adithya Shenoy <adithyashenoy6@gmail.com>
2022-06-22 11:09:34 +0530

    Entitlement script addded

---
+++ Entitlement/dml_script_22-6-2022-11-9-32.sql
@@ -0,0 +1,8 @@
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='CareMgmt-CondMgmt-Immunizations') and RGN_ID in ('MID');
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='CareMgmt-ClinDxHealthCond') and RGN_ID in ('SCA');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=CareMgmt-CondMgmt-Immunizations), 'MID', 'N');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=CareMgmt-ClinDxHealthCond), 'SCA', 'N');
+
+commit;
