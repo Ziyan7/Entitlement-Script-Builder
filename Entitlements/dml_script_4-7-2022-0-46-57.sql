a6f7640be1878fc4645e08c927d311e4d0162a76
Adithya S Shenoy <62694257+AadiShenoy@users.noreply.github.com>
2022-07-04 12:47:02 +0530

    Entitlement script addded

---
+++ Entitlements/dml_script_4-7-2022-0-46-57.sql
@@ -0,0 +1,9 @@
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='Administrative-StaffAdministered-HCOFeatures') and RGN_ID in ('CO','DB','CS');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=Administrative-StaffAdministered-HCOFeatures), 'CO', 'N');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=Administrative-StaffAdministered-HCOFeatures), 'DB', 'N');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=Administrative-StaffAdministered-HCOFeatures), 'CS', 'N');
+
+commit;
