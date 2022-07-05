fb39efecea015ba355cf4de7c5423428102f277a
Adithya S Shenoy <62694257+AadiShenoy@users.noreply.github.com>
2022-07-05 16:53:03 +0530

    Entitlement script addded

---
+++ Entitlements/dml_script_7-5-2022--4-53-01-PM.sql
@@ -0,0 +1,17 @@
+/*********************************************************************************************************/
+-- <APPLICATION_NAME>Questionnaires</APPLICATION_NAME>
+-- <RELEASE> 5.2</RELEASE>
+-- <DEVELOPER>Adithya S Shenoy</DEVELOPER>
+-- <PROD_SCHEMA_NAME>HINT1</PROD_SCHEMA_NAME>
+-- <TRANSACTION_TYPE>DML</TRANSACTION_TYPE>
+-- <OPERATION_TYPE>DELETE,INSERT</OPERATION_TYPE>
+-- <AFFECTED_TABLES>DELETE:WPP_CFE_PRVLG_FOR_RGN, INSERT:WPP_CFE_PRVLG_FOR_RGN</AFFECTED_TABLES>
+-- <JIRA>https://jira-aes.kp.org/browse/RM-12345</JIRA>
+-- <SHORTID>232</SHORTID>
+/*********************************************************************************************************/
+
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='Questionnaires') and RGN_ID in ('MID');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=Questionnaires), 'MID', 'N');
+
+commit;
