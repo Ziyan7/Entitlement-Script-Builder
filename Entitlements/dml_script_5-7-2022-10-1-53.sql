2247950c61a6876c7d32e8ebb3a40587882998a6
Adithya S Shenoy <62694257+AadiShenoy@users.noreply.github.com>
2022-07-05 10:01:55 +0530

    Entitlement script addded

---
+++ Entitlements/dml_script_5-7-2022-10-1-53.sql
@@ -0,0 +1,17 @@
+/*********************************************************************************************************/
+    -- <APPLICATION_NAME>Diagnostics-ClinDx-Allergies</APPLICATION_NAME>
+    -- <RELEASE> 5.2</RELEASE>
+    -- <DEVELOPER>Adithya S Shenoy</DEVELOPER>
+    -- <PROD_SCHEMA_NAME>HINT1,HPPIDC,HREG1</PROD_SCHEMA_NAME>
+    -- <TRANSACTION_TYPE>DML</TRANSACTION_TYPE>
+    -- <OPERATION_TYPE>DELETE,INSERT</OPERATION_TYPE>
+    -- <AFFECTED_TABLES>DELETE:WPP_CFE_PRVLG_FOR_RGN, INSERT:WPP_CFE_PRVLG_FOR_RGN</AFFECTED_TABLES>
+    -- <JIRA>https://jira-aes.kp.org/browse/RM-43065</JIRA>
+    -- <SHORTID>505</SHORTID>
+    /*********************************************************************************************************/
+
+delete from WPP_ENTL_PRMSN_OVRD where ENTL_NM='Diagnostics-ClinDx-Allergies' and guid in ("12345");
+
+INSERT INTO WPP_ENTL_PRMSN_OVRD (WPP_ENTL_PRMSN_OVRD_IK,RGN,GUID,ENTL_PRMSN,ENTL_NM,ENTL_SRC_ID,PILOT_APP_NAME,CRTE_DT_TM,MOD_DT_TM)VALUES (null,'Southern California (SCA)','12345','DENY','Diagnostics-ClinDx-Allergies','KP_EBIZ','Diagnostics-ClinDx-Allergies',SYSTIMESTAMP,SYSTIMESTAMP);
+
+commit;
