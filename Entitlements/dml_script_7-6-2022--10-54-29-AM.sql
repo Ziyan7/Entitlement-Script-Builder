d81747fca7834a12f9c96d504a456c8b201cf0b5
Adithya S Shenoy <62694257+AadiShenoy@users.noreply.github.com>
2022-07-06 10:54:31 +0530

    Entitlement script addded

---
+++ Entitlements/dml_script_7-6-2022--10-54-29-AM.sql
@@ -0,0 +1,19 @@
+/*********************************************************************************************************/
+-- <APPLICATION_NAME>CareMgmt-ClinDxHealthCond</APPLICATION_NAME>
+-- <RELEASE> 5.2</RELEASE>
+-- <DEVELOPER>Adithya S Shenoy</DEVELOPER>
+-- <PROD_SCHEMA_NAME>HINT1</PROD_SCHEMA_NAME>
+-- <TRANSACTION_TYPE>DML</TRANSACTION_TYPE>
+-- <OPERATION_TYPE>DELETE,INSERT</OPERATION_TYPE>
+-- <AFFECTED_TABLES>DELETE:WPP_CFE_PRVLG_FOR_RGN, INSERT:WPP_CFE_PRVLG_FOR_RGN</AFFECTED_TABLES>
+-- <JIRA>https://jira-aes.kp.org/browse/https://jira-aes.kp.org/browse/RM-43128</JIRA>
+-- <SHORTID>207</SHORTID>
+/*********************************************************************************************************/
+
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='CareMgmt-ClinDxHealthCond') and RGN_ID in ('SCA','HAW');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=CareMgmt-ClinDxHealthCond), 'SCA', 'N');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=CareMgmt-ClinDxHealthCond), 'HAW', 'N');
+
+commit;
