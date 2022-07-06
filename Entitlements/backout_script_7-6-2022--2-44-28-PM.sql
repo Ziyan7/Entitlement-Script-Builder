207f7e67f17e0820297609bbaf046eca1204a1aa
Adithya S Shenoy <62694257+AadiShenoy@users.noreply.github.com>
2022-07-06 14:44:32 +0530

    Entitlement script addded

---
+++ Entitlements/backout_script_7-6-2022--2-44-28-PM.sql
@@ -0,0 +1,14 @@
+/*********************************************************************************************************/
+-- <APPLICATION_NAME>Personal Action Plan</APPLICATION_NAME>
+-- <RELEASE> 5.2</RELEASE>
+-- <DEVELOPER>Adithya S Shenoy</DEVELOPER>
+-- <PROD_SCHEMA_NAME>efedgs</PROD_SCHEMA_NAME>
+-- <TRANSACTION_TYPE>DML</TRANSACTION_TYPE>
+-- <OPERATION_TYPE>DELETE,INSERT</OPERATION_TYPE>
+-- <AFFECTED_TABLES>DELETE:WPP_CFE_PRVLG_FOR_RGN, INSERT:WPP_CFE_PRVLG_FOR_RGN</AFFECTED_TABLES>
+-- <JIRA>https://jira-aes.kp.org/browse/fdssds</JIRA>
+-- <SHORTID>234</SHORTID>
+/*********************************************************************************************************/
+
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='Personal Action Plan') and RGN_ID in ('SCA','GGA');
+commit;
