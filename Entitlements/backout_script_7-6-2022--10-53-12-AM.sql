acffd5c69d17288c0e6a12bbb84022e5308ece1a
Adithya S Shenoy <62694257+AadiShenoy@users.noreply.github.com>
2022-07-06 10:53:15 +0530

    Entitlement script addded

---
+++ Entitlements/backout_script_7-6-2022--10-53-12-AM.sql
@@ -0,0 +1,14 @@
+/*********************************************************************************************************/
+-- <APPLICATION_NAME>CareMgmt-ClinDxHealthCond</APPLICATION_NAME>
+-- <RELEASE> 5.2</RELEASE>
+-- <DEVELOPER>Adithya S Shenoy</DEVELOPER>
+-- <PROD_SCHEMA_NAME>HINT1</PROD_SCHEMA_NAME>
+-- <TRANSACTION_TYPE>DML</TRANSACTION_TYPE>
+-- <OPERATION_TYPE>DELETE,INSERT</OPERATION_TYPE>
+-- <AFFECTED_TABLES>DELETE:WPP_CFE_PRVLG_FOR_RGN, INSERT:WPP_CFE_PRVLG_FOR_RGN</AFFECTED_TABLES>
+-- <JIRA>https://jira-aes.kp.org/browse/saghbfsd</JIRA>
+-- <SHORTID>207</SHORTID>
+/*********************************************************************************************************/
+
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='CareMgmt-ClinDxHealthCond') and RGN_ID in ('KNW','GGA');
+commit;
