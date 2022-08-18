db73ddd21a2c62f51e425ee16679379c985a1458
Adithya S Shenoy <62694257+AadiShenoy@users.noreply.github.com>
2022-08-18 11:21:24 +0200

    Entitlement script addded

---
+++ Entitlements/Automated-Entitlement/Aug-2022/test_dml_8-18-2022--2-51-22-PM.sql
@@ -0,0 +1,21 @@
+/*********************************************************************************************************/
+-- <APPLICATION_NAME>CareMgmt-ClinDxHealthCond</APPLICATION_NAME>
+-- <RELEASE> 5.2</RELEASE>
+-- <DEVELOPER>Adithya S Shenoy</DEVELOPER>
+-- <PROD_SCHEMA_NAME>sax</PROD_SCHEMA_NAME>
+-- <TRANSACTION_TYPE>DML</TRANSACTION_TYPE>
+-- <OPERATION_TYPE>DELETE,INSERT</OPERATION_TYPE>
+-- <AFFECTED_TABLES>DELETE:WPP_CFE_PRVLG_FOR_RGN, INSERT:WPP_CFE_PRVLG_FOR_RGN</AFFECTED_TABLES>
+-- <JIRA>asx</JIRA>
+-- <SHORTID>207</SHORTID>
+/*********************************************************************************************************/
+
+delete from WPP_CFE_PRVLG_FOR_RGN where CFE_ID=(select cfe_id from wpp_cfe where CFE_NM='CareMgmt-ClinDxHealthCond') and RGN_ID in ('CO','DB','CS');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=CareMgmt-ClinDxHealthCond), 'CO', 'Y');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=CareMgmt-ClinDxHealthCond), 'DB', 'Y');
+
+INSERT INTO WPP_CFE_PRVLG_FOR_RGN ( CFE_ID, RGN_ID, DIS_IN ) VALUES ( (select cfe_id from wpp_cfe where CFE_NM=CareMgmt-ClinDxHealthCond), 'CS', 'Y');
+
+commit;
